34c5e1ea8065f6eb17cf250e8a9ba4c2203e4710
erhanbilir <eturanbilir@outlook.com>
2025-07-17 15:55:59 +0300

    IOC updated (push and pull button)

--- software/IOC.X/main.c
+++ software/IOC.X/main.c
@@ -10,6 +10,9 @@
 
 #define _XTAL_FREQ 64000000
 
+volatile char previous_PORTB_RB4 = 0;
+volatile char previous_PORTB_RB5 = 0;
+
 void main(void) {
     OSCCONbits.IRCF = 0b111;
     OSCTUNEbits.PLLEN = 0b1;
@@ -47,11 +50,39 @@ void __interrupt(high_priority) ISR_high()
 {
     if(INTCONbits.RBIF == 1 && INTCONbits.RBIE == 1)
     {
-        LATDbits.LATD2 = 1;
-        LATDbits.LATD3 = 0;
-        __delay_ms(500);
-        PORTB;
-        NOP();
+        /*!< Delay for debouncing */
+        __delay_ms(50);
+        
+        /*!< Read the current PORTB */
+        char current_PORTB = PORTB;
+        
+        /*!< Extract the RB5 and RB4 pins from current PORTB*/
+        char current_PORTB_RB5 = (current_PORTB & 0b00100000) >> 5;
+        char current_PORTB_RB4 = (current_PORTB & 0b00010000) >> 4;
+        
+        /*!< If a pin went from low to high state*/
+        if (previous_PORTB_RB5 == 0 && current_PORTB_RB5 == 1  \
+            || previous_PORTB_RB4 == 0 && current_PORTB_RB4 == 1)
+        {
+            LATDbits.LATD2 = 1;
+            LATDbits.LATD3 = 0;
+        }
+        else if (previous_PORTB_RB5 == 1 && current_PORTB_RB5 == 0 \
+                 || previous_PORTB_RB4 == 1 && current_PORTB_RB4 == 0 )
+        {
+            LATDbits.LATD2 = 0;
+            LATDbits.LATD3 = 1;
+        }
+        
+        __delay_ms(1000);
+
+        
+        /*!< Read the current PORTB and Extract the RB5 and RB4 pins from current PORTB */
+        current_PORTB = PORTB;
+        previous_PORTB_RB5 = (current_PORTB & 0b00100000) >> 5;
+        previous_PORTB_RB4 = (current_PORTB & 0b00010000) >> 4;
+        
+        
 
         INTCONbits.RBIF = 0;
     }
