17b94816b188977ae89082373eb86cd1a8996bed
erhanbilir <eturanbilir@outlook.com>
2025-07-23 16:02:37 +0300

    UART Echo interrupt method

--- software/UART.X/main.c
+++ software/UART.X/main.c
@@ -10,6 +10,9 @@
 #include "stdint.h"
 #include "stdbool.h"
 
+/**
+ * @brief Structure for UART configuration parameters
+ */
 typedef struct
 {
     uint8_t gen_reg;
@@ -17,11 +20,12 @@ typedef struct
     uint8_t brgh;
 }UART_Init_TypeDef;
 
+/*!< Function prototypes */
 void __interrupt() ISR(void);
 void ISR_Init(void);
 void UART_Init(UART_Init_TypeDef *UART_ConfigStruct);
-void UART_Send(uint8_t data);
-void UART_Receive();
+void UART_Send(uint8_t *data);
+void UART_Receive(uint8_t *data);
 
 uint8_t rx_data;
 
@@ -32,15 +36,16 @@ void main(void) {
     UART1.gen_reg = 25;
     
     UART_Init(&UART1);
+    ISR_Init();
     
-    while(1)
-    {
-        __delay_ms(200);
-        UART_Send('A');
-
-    }
+    while(1);
+    
     return;
 }
+
+/** 
+ * @brief Enables global and peripheral interrupts and enables UART RX interrupt
+ */
 void ISR_Init(void)
 {
     INTCONbits.GIE = 1;
@@ -49,6 +54,10 @@ void ISR_Init(void)
     PIE1bits.RCIE = 1;
 }
 
+/**
+ * @brief Configures the UART hardware using provided settings
+ * @param UART_ConfigStruct Pointer to configuration structure
+ */
 void UART_Init(UART_Init_TypeDef *UART_ConfigStruct)
 {
     TRISCbits.TRISC6 = 0;
@@ -64,20 +73,46 @@ void UART_Init(UART_Init_TypeDef *UART_ConfigStruct)
     TXSTAbits.TXEN = 1;
 }
 
+/**
+ * @brief Main Interrupt Service Routine
+ * Handles UART RX interrupts and echoes received data.
+ */
 void __interrupt() ISR(void)
 {
     if(PIR1bits.RCIF)
     {
-        rx_data = RCREG;
-    }
-    if(PIR1bits.TXIF && PIE1bits.TXIE)
-    {
+        UART_Receive(&rx_data);
+        UART_Send(&rx_data);
     }
 }
 
-void UART_Send(uint8_t data)
+/**
+ * @brief Sends a single byte over UART
+ * @param data Pointer to byte to be transmitted
+ */
+void UART_Send(uint8_t *data)
 {
     while(!TXSTAbits.TRMT);
-    TXREG = data;
+    TXREG = *data;
 }
 
+/**
+ * @brief Reads one byte from UART and handles errors
+ * @param data Pointer to variable where received byte will be stored
+ */
+void UART_Receive(uint8_t *data)
+{
+    if(RCSTAbits.OERR)
+    {
+        RCSTAbits.CREN = 0;
+        RCSTAbits.CREN = 1;
+    }
+    else if(RCSTAbits.FERR)
+    {
+        uint8_t err = RCREG;
+    }
+    else
+    {
+        *data = RCREG;
+    }
+}
