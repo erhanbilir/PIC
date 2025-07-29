a37e4726acb64e6031e626d69583f7c8b8e8a634
erhanbilir <eturanbilir@outlook.com>
2025-07-29 15:40:33 +0300

    feat(pwm): add basic PWM example with dynamic duty cycle update

--- software/PWM.X/main.c
+++ software/PWM.X/main.c
@@ -8,31 +8,68 @@
 #include "config.h"
 
 void IO_Init(void);
-void PWM_Init(uint8_t duty);
+void PWM_Init(void);
+void PWM_SetDuty(uint16_t duty);
+
+/*!< PWM Period = [(PR2) + 1] ? 4 ? TOSC ? (TMR2 Prescale Value)               */
+/*!<          ==> 100 ? 4 ? 125 ns ? 4 = 200 ?s                                */
+/*!< PWM Duty Cycle =(CCPR1L:CCP1CON<5:4>) ? TOSC ? (TMR2 Prescale Value)      */
+/*!<          ==> [value] ? 125 ns ? 4                                         */
+/*!< Duty cycle ratio = value / (4 * (PR2 + 1))                               */
+/*!< value = 100  =>  25% duty => ~1.25V average (assuming 5V supply)         */
+/*!< value = 399  => ~99.75% duty => ~5V average (assuming 5V supply)         */
 
 void main(void) {
     IO_Init();
-    PWM_Init(100);
+    PWM_Init();
     
-    while(1);
+    uint16_t duty = 0;
+    while(1)
+    {
+        PWM_SetDuty(duty);
+        __delay_ms(20);
+        duty++;
+        if(duty >= 399)
+        {
+            duty = 0;
+        }
+    }
     
     return;
 }
 
+/**
+ * @brief Configures I/O ports
+ * RC2 pin (CCP1 output) is set as output
+ */
 void IO_Init(void)
 {
     TRISC = 0x00;
     PORTC = 0x00;
 }
 
-void PWM_Init(uint8_t duty)
+/**
+ * @brief Initializes the PWM module
+ * CCP1 is set to PWM mode and Timer2 is enabled
+ */
+void PWM_Init(void)
 {
-    CCP1CON = 0b00001111;
-    T2CON = 0b00000101;
-    PR2 = 99;
-    TMR2 = 0;
-    
+    CCP1CON = 0b00001111;   /*!< CCP1 in PWM mode        */
+    T2CON = 0b00000101;     /*!< Timer ON, Prescaler = 4 */
+    PR2 = 99;               /*!< Set PWM period          */
+    TMR2 = 0;               /*!< Clear Timer2            */
+}
+
+/**
+ * @brief Sets the PWM duty cycle
+ * @param duty 10-bit duty cycle value (0 - 399)
+ */
+void PWM_SetDuty(uint16_t duty)
+{
+    /*!< Lower 2 bits */
     CCP1CONbits.CCP1Y = (duty & 0x1);
     CCP1CONbits.CCP1X = (duty & 0x2);
+    
+    /*!< Higher 8 bits */
     CCPR1L = duty >> 2;
 }
