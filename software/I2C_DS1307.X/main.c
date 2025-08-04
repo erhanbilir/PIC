809d8819ab0a410284c4dcf6ca8c89a30f389ded
erhanbilir <eturanbilir@outlook.com>
2025-08-04 14:09:59 +0300

    feat(i2c-rtc): add full DS1307 date/time read and write support using bitfield struct

--- software/I2C_DS1307.X/main.c
+++ software/I2C_DS1307.X/main.c
@@ -16,6 +16,39 @@
 uint8_t uart_data = 0;
 char print_buffer[32] = {0};
 
+typedef struct{
+    
+    uint8_t secL:4;
+    uint8_t secH:3;
+    uint8_t hold_clock:1;
+    
+    uint8_t minL:4;
+    uint8_t minH:3;
+    uint8_t rfu1:1;
+    
+    uint8_t hourL:4;
+    uint8_t hourH:2;
+    uint8_t hour_mode:1;
+    uint8_t rfu2:1;
+    
+    uint8_t day:3;
+    uint8_t rfu3:5;
+    
+    uint8_t dateL:4;
+    uint8_t dateH:2;
+    uint8_t rfu4:2;
+    
+    uint8_t monthL:4;
+    uint8_t monthH:1;
+    uint8_t rfu5:3;
+    
+    uint8_t yearL:4;
+    uint8_t yearH:4;
+
+}rtc_time_read;
+
+rtc_time_read rtc_data;
+
 void __interrupt() ISR(void);
 
 extern void ISR_Init(void);
@@ -34,37 +67,94 @@ void I2C_Wait(void);
 
 void main(void) {
     __delay_ms(200);
-    
+    uint8_t rtc_buff[7] = {0};
     UART_Init_TypeDef UART1 = {.sync = 0, .brgh = 1, .gen_reg = 25};
     UART_Init(&UART1);
     __delay_ms(200);
-
+    
+    memset(&rtc_data,0,sizeof(rtc_data));
+    
+    rtc_data.secH = 0;
+    rtc_data.secH = 0;
+    
+    rtc_data.minL = 5;
+    rtc_data.minH = 1;
+    
+    rtc_data.hourL = 3;
+    rtc_data.hourH = 2;
+    
+    rtc_data.day = 7;
+    
+    rtc_data.dateL = 7;
+    rtc_data.dateH = 1;
+    
+    rtc_data.monthL = 1;
+    rtc_data.monthH = 1;
+    
+    rtc_data.yearL = 8;
+    rtc_data.yearH = 1;
+    
+    memcpy(rtc_buff, &rtc_data,sizeof(rtc_data));
+    
     sprintf(print_buffer, "\n\rprogram start\n\r");
     UART_WriteString(print_buffer);
     ISR_Init();
+    
     I2C_Init();
-    uint8_t sec = 0;
+    
+    I2C_Start();
+    I2C_Write(RTC_WRITE_ADDR);
+    I2C_Write(0x00);
+    I2C_Write(rtc_buff[0]);
+    I2C_Write(rtc_buff[1]);
+    I2C_Write(rtc_buff[2]);
+    I2C_Write(rtc_buff[3]);
+    I2C_Write(rtc_buff[4]);
+    I2C_Write(rtc_buff[5]);
+    I2C_Write(rtc_buff[6]);
+    I2C_Stop();
+    
+    sprintf(print_buffer, "\n\r");
+    UART_WriteString(print_buffer);
+    
     while(1)
     {
+
+        
+
         I2C_Start();
         I2C_Write(RTC_WRITE_ADDR);
         I2C_Write(0x00);
         I2C_Restart();
         I2C_Write(RTC_READ_ADDR);
-        sec = I2C_Read(1);
+        rtc_buff[0] = I2C_Read(0);
+        rtc_buff[2] = I2C_Read(0);
+        rtc_buff[3] = I2C_Read(0);
+        rtc_buff[4] = I2C_Read(0);
+        rtc_buff[5] = I2C_Read(0);
+        rtc_buff[6] = I2C_Read(0);
+        rtc_buff[7] = I2C_Read(1);
         I2C_Stop();
-        sprintf(print_buffer, "sec: 0x%02x\n\r",sec);
+        
+        memcpy(&rtc_data,rtc_buff,sizeof(rtc_data));   
+        sprintf(print_buffer,"\r20%d%d/%d%d/%d%d %d%d:%d%d:%d%d",
+                rtc_data.yearH,
+                rtc_data.yearL,
+                rtc_data.monthH,
+                rtc_data.monthL,
+                rtc_data.dateH,
+                rtc_data.dateL,
+                rtc_data.hourH,
+                rtc_data.hourL,
+                rtc_data.minH,
+                rtc_data.minL,
+                rtc_data.secH,
+                rtc_data.secL
+                );
     
         UART_WriteString(print_buffer);
-        __delay_ms(1000);
+        __delay_ms(500);
     }
-
-
-    
-
-
-    while(1);
-    
     return;
 }
 
